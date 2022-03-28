cf894bf520b15389d3361f8b3f4872092451f113
Eugene Puzenko <puzzenko89@gmail.com>
2022-03-29 01:25:56 +0300

    it was corrected

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
+const btnShowMore = document.querySelector('.button-show-more');
+const blockMore = document.querySelector('.more');
+const btnShowMoreDown = document.querySelector('.button-show-more--active');
+
+btnShowMore.addEventListener ('click', function (){
+    if (blockMore.style.display === 'grid') {
+        blockMore.style.display = 'none';
+        btnShowMore.innerHTML = 'Показать всё';
+        btnShowMore.classList.toggle('button-show-more');
+    } else {
+        blockMore.style.display = 'grid';
+        btnShowMore.innerHTML = 'Скрыть';
+        btnShowMoreDown.classList.toggle('button-show-more');
+    }
+});
+
+
+
 const breakpoint = window.matchMedia( '(min-width:500px)' );
   let mySwiper;
 
   const breakpointChecker = () => {
-    if ( breakpoint.matches === true ) {
-        if ( mySwiper !== undefined ) {
-          mySwiper.destroy( true, true );
-        }
-        return;
-    } else if ( breakpoint.matches === false ) {
-        return enableSwiper();
-    }
-  };
+    if (breakpoint.matches) {
+      mySwiper.destroy( true, true );
+    } else {
+     enableSwiper()
+  }};
+
   const enableSwiper = () => {
     mySwiper = new Swiper ('.swiper', {
       direction: 'horizontal',
@@ -38,21 +53,3 @@ const breakpoint = window.matchMedia( '(min-width:500px)' );
 breakpoint.addEventListener('change', breakpointChecker);
 breakpointChecker();
 
-let btnShowMore = document.querySelector('.button-show-more');
-let blockMore = document.querySelector('.more');
-let btnShowMoreDown = document.querySelector('.button-show-more--active');
-
-
-btnShowMore.addEventListener ('click', function (){
-    if (blockMore.style.display === 'grid') {
-        blockMore.style.display = 'none';
-        btnShowMore.innerHTML = 'Показать всё';
-        btnShowMore.classList.remove('button-show-more--active');
-        btnShowMore.classList.add('button-show-more');
-    } else {
-        blockMore.style.display = 'grid';
-        btnShowMore.innerHTML = 'Скрыть';
-        btnShowMore.classList.remove('button-show-more');
-        btnShowMore.classList.add('button-show-more--active');
-    }
-});
